46b7a15b064e9468a6de111fade007848f381d4a
Joan Goyeau <jgoyeau@netflix.com>
2026-04-17 01:11:27 -0400

    Add toLong for numerics (#2)

--- src/com/netflix/wick/functions/toLong.scala
+++ src/com/netflix/wick/functions/toLong.scala
@@ -3,8 +3,18 @@ package functions
 
 import org.apache.spark.sql.catalyst.expressions.*
 import org.apache.spark.sql.types.DataTypes
+import scala.annotation.targetName
+import com.netflix.wick.column.Numeric
 
 extension (string: Expr[String]) def toLong: LinearExpr[Long] = LinearExpr(Cast(string.underlying, DataTypes.LongType))
 
 extension (string: ScalarExpr[String])
   def toLong: ScalarExpr[Long] = ScalarExpr(Cast(string.underlying, DataTypes.LongType))
+
+extension (numeric: Expr[Numeric])
+  @targetName("numericToLong")
+  def toLong: LinearExpr[Long] = LinearExpr(Cast(numeric.underlying, DataTypes.LongType))
+
+extension (numeric: ScalarExpr[Numeric])
+  @targetName("numericToLong")
+  def toLong: ScalarExpr[Long] = ScalarExpr(Cast(numeric.underlying, DataTypes.LongType))
--- test/src/com/netflix/wick/functions/toLongTest.scala
+++ test/src/com/netflix/wick/functions/toLongTest.scala
@@ -6,6 +6,7 @@ import com.netflix.wick.{*, given}
 import com.netflix.wick.SparkSuite
 import com.netflix.wick.column.lit
 import com.netflix.wick.model.*
+import java.math.BigDecimal
 
 class toLongTest extends FunSuite with SparkSuite:
 
@@ -46,3 +47,57 @@ class toLongTest extends FunSuite with SparkSuite:
       result.dataFrame.collect().map(_.getAs[Long]("n")).toSeq,
       Seq(Int.MaxValue.toLong + 1)
     )
+
+  test("toLong truncates Double column to Long"):
+    val data = spark.createDataSeq(Seq(Transaction("a", 3.7), Transaction("b", -1.9), Transaction("c", 100.0)))
+
+    val result = data.select(row => (n = row.amount.toLong))
+
+    assertEquals(result.dataFrame.schema, StructType(Array(StructField("n", LongType, nullable = true))))
+    assertEquals(
+      result.dataFrame.collect().map(_.getAs[Long]("n")).toSeq,
+      Seq(3L, -1L, 100L)
+    )
+
+  test("toLong on ScalarExpr[Double] via lit truncates to Long"):
+    val data = spark.createDataSeq(Seq(StringRecord("x")))
+
+    val result = data.select(_ => (n = lit(42.9).toLong))
+
+    assertEquals(result.dataFrame.collect().map(_.getAs[Long]("n")).toSeq, Seq(42L))
+
+  test("toLong converts Int column to Long"):
+    val data = spark.createDataSeq(Seq(Department(1, "Engineering"), Department(2, "Marketing")))
+
+    val result = data.select(row => (n = row.id.toLong))
+
+    assertEquals(result.dataFrame.schema, StructType(Array(StructField("n", LongType, nullable = true))))
+    assertEquals(result.dataFrame.collect().map(_.getAs[Long]("n")).toSeq, Seq(1L, 2L))
+
+  test("toLong on ScalarExpr[Int] via lit converts to Long"):
+    val data = spark.createDataSeq(Seq(StringRecord("x")))
+
+    val result = data.select(_ => (n = lit(5).toLong))
+
+    assertEquals(result.dataFrame.collect().map(_.getAs[Long]("n")).toSeq, Seq(5L))
+
+  test("toLong on ScalarExpr[Long] via lit is identity"):
+    val data = spark.createDataSeq(Seq(StringRecord("x")))
+
+    val result = data.select(_ => (n = lit(10L).toLong))
+
+    assertEquals(result.dataFrame.collect().map(_.getAs[Long]("n")).toSeq, Seq(10L))
+
+  test("toLong on ScalarExpr[Float] via lit truncates to Long"):
+    val data = spark.createDataSeq(Seq(StringRecord("x")))
+
+    val result = data.select(_ => (n = lit(2.5f).toLong))
+
+    assertEquals(result.dataFrame.collect().map(_.getAs[Long]("n")).toSeq, Seq(2L))
+
+  test("toLong on ScalarExpr[BigDecimal] via lit truncates to Long"):
+    val data = spark.createDataSeq(Seq(StringRecord("x")))
+
+    val result = data.select(_ => (n = lit(BigDecimal("42.9")).toLong))
+
+    assertEquals(result.dataFrame.collect().map(_.getAs[Long]("n")).toSeq, Seq(42L))
